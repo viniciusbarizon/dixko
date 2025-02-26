1c14f4f4836a07c066d1af8e2c57570db305120e
Vinicius Barizon <viniciusbarizon@gmail.com>
2025-02-26 20:13:59 +0000

    telescope

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,5 +3,6 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\FolioServiceProvider::class,
+    App\Providers\TelescopeServiceProvider::class,
     App\Providers\VoltServiceProvider::class,
 ];
