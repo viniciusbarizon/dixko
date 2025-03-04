d650315b8309c299df6e03d4d7893fa64c35ad80
viniciusbarizon <1687062+viniciusbarizon@users.noreply.github.com>
2025-03-04 17:18:42 +0000

    PHP Linting (Pint)

--- app/Http/Controllers/Index.php
+++ app/Http/Controllers/Index.php
@@ -1,10 +1,12 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Http\Controllers;
 
 use Illuminate\View\View;
 
-class Index extends Controller
+final class Index extends Controller
 {
     /**
      * Handle the incoming request.
